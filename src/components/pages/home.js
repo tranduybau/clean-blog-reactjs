dc8af226d0974d996771c03a791723281401becc
bautd-2046 <tran.duy.bau@sun-asterisk.com>
2019-09-12 16:36:08 +0700

    add env file

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 
-import { fetchAllPostsFromApi } from "dispatchers'";
+import { fetchAllPostsFromApi } from "dispatchers";
 
 class Home extends Component {
 	constructor(props) {
